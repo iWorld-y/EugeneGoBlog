1cdeff122e486c828855c1ccbad584a7497dd4ff
Eugene <caima12138@foxmail.com>
2024-02-25 22:37:42 +0800

    项目搭建

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type IndexData struct {
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	indexData := IndexData{
+		Title: "Eugene 的博客",
+		Desc:  "入门学习作业",
+	}
+	jsonStr, _ := json.Marshal(indexData)
+	w.Write(jsonStr)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/", index)
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+}
