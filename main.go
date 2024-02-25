68f8221121675bba7068a9423e09a2918889e180
Eugene <caima12138@foxmail.com>
2024-02-25 22:50:02 +0800

    P3 模板页面

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -21,11 +23,23 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonStr)
 }
 
+func indexHTML(w http.ResponseWriter, r *http.Request) {
+	indexData := IndexData{
+		Title: "Eugene 的博客",
+		Desc:  "入门学习作业",
+	}
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexData)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHTML)
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
