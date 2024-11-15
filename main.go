e7de6784db3df88a140f3bf3075a614c3df605d8
iWorld-y <caima12138@foxmail.com>
2024-11-15 16:01:12 +0000

    feat: hello, world!

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func main() {
+	restConf := &rest.RestConf{}
+	conf.MustLoad("etc/helloworld.yaml", restConf)
+	s, err := rest.NewServer(*restConf)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	s.AddRoute(rest.Route{
+		Method: http.MethodGet,
+		Path:   "/",
+		Handler: func(rw http.ResponseWriter, r *http.Request) {
+			httpx.OkJson(rw, "Hello, World! ")
+		},
+	})
+	defer s.Stop()
+	s.Start()
+}
